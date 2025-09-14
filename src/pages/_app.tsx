c17a7fb68d6026405f36f846c11a2b2f7b81f5e6
Daniel Wohlgemuth <daniel.wohlgemuth.epp@gmail.com>
2025-09-14 12:06:02 -0300

    Add initial authetication handling

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,15 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
+import { AuthProvider } from "react-oidc-context";
+
+const cognitoAuthConfig = {
+  authority: process.env.NEXT_PUBLIC_COGNITO_AUTHORITY,
+  client_id: process.env.NEXT_PUBLIC_COGNITO_CLIENT_ID,
+  redirect_uri: process.env.NEXT_PUBLIC_REDIRECT_SIGN_IN,
+  response_type: "code",
+  scope: "email openid profile",
+};
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return <AuthProvider {...cognitoAuthConfig}><Component {...pageProps} /></AuthProvider>;
 }
